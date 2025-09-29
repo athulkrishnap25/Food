fce909afbe3c5eaa84b361d2eb506e069e30f4c1
athulkrishnap25 <athulkrishnapjio@gmail.com>
2025-09-29 12:17:44 +0530

    Hello user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,32 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext.jsx";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const { name } = useContext(UserContext);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
-      <h1 className="text-4xl font-bold mb-4">
-        Hello, {name ? name : "Guest"}! 👋
-      </h1>
-      <p className="text-lg">Welcome to the QuickBite</p>
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+
+    return (
+    <div className="relative h-screen w-screen">
+      <div
+        className={`fixed inset-0 flex items-center justify-center bg-gradient-to-r from-gray-900 to-black text-white text-4xl font-bold transition-opacity duration-1000 ${
+          showSplash ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        Hello, {name ? name : "Guest"}!
+      </div>
+      <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
+        <h1 className="text-3xl font-semibold">Welcome to Main Page 🚀</h1>
+      </div>
     </div>
   );
 }
