de3e86ee093b869054fa440102f215984858d235
athulkrishnap25 <athulkrishnapjio@gmail.com>
2025-10-01 20:58:17 +0530

    Categories spacing updated

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CategoryCard({ image, name }) {
   return (
-    <div className="flex flex-col items-center shadow-md rounded-xl p-4 w-100">
+    <div className="flex flex-col items-center shadow-md rounded-xl p-4 w-60">
       <div className="w-40 h-40 rounded-full overflow-hidden mb-2">
         <img src={image} alt={name} className="w-full h-full object-cover" />
       </div>
